9d1c2798dba896283baf998703f6ec1d0c756086
JakubKJ <jakub.k.jasinski@student.put.poznan.pl>
2020-04-30 13:28:52 +0200

    Create RiskProperty MVC

--- backend/RiskManagementAPI/Models/RiskManagementDbContext.cs
+++ backend/RiskManagementAPI/Models/RiskManagementDbContext.cs
@@ -23,5 +23,7 @@ namespace RiskManagementAPI.Models
         public DbSet<RiskManagementAPI.Models.User> User { get; set; }
         
         public DbSet<RiskManagementAPI.Models.Project> Project { get; set; }
+        
+        public DbSet<RiskManagementAPI.Models.RiskProperty> RiskProperty { get; set; }
     }
 }
