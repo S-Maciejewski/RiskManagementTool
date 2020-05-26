7413f8a34be01eb4109dc5b58b3991619b97c5b7
PiterPSP <piter-h20@wp.pl>
2020-05-27 01:39:49 +0200

    project.ts refactored, empty constructor added (optional params)

--- frontend/riskManagementTool/src/app/model/project.ts
+++ frontend/riskManagementTool/src/app/model/project.ts
@@ -1,14 +1,14 @@
 export class Project {
 
-  constructor(id: number, name: string, description: string, riskRegisters: object) {
+  public id: number;
+  public name: string;
+  public description: string;
+  public riskRegisters: object;
+
+  constructor(id?: number, name?: string, description?: string, riskRegisters?: object) {
     this.id = id;
     this.name = name;
     this.description = description;
     this.riskRegisters = riskRegisters;
   }
-
-  public id: number;
-  public name: string;
-  public description: string;
-  public riskRegisters: object;
 }
