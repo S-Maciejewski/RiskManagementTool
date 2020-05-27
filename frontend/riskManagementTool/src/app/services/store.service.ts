43fdfb945c36acf7111fc57d153a0aeb8067479f
PiterPSP <piter-h20@wp.pl>
2020-05-27 02:47:05 +0200

    added fetching impact enum after login

--- frontend/riskManagementTool/src/app/services/store.service.ts
+++ frontend/riskManagementTool/src/app/services/store.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { ApiGetService } from "./api-get.service";
 
 
 // Service for storing common immutable data (i.e. database enums)
@@ -7,5 +8,19 @@ import { Injectable } from '@angular/core';
 })
 export class StoreService {
 
-  constructor() { }
+  constructor(private apiGetService: ApiGetService) { }
+
+  public impacts: object;
+
+  getEnums() {
+    this.fetchImpacts();
+    //todo severity, probability
+  }
+
+  fetchImpacts() {
+    //todo real endpoint url
+    this.apiGetService.get('http://www.mocky.io/v2/5ecdb6943000006800ea0be6').subscribe( res => {
+      this.impacts = JSON.parse(JSON.stringify(res));
+    });
+  }
 }
