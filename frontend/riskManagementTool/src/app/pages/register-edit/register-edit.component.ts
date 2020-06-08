dce2d2f6c9fa8bdd2afd94f11b8fcd82f63dc186
PiterPSP <piter-h20@wp.pl>
2020-06-09 01:19:08 +0200

    implemented register edit

--- frontend/riskManagementTool/src/app/pages/register-edit/register-edit.component.ts
+++ frontend/riskManagementTool/src/app/pages/register-edit/register-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RiskRegister } from 'src/app/model/riskRegister';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RiskRegistersService } from 'src/app/services/risk-registers.service';
 
 @Component({
   selector: 'app-register-edit',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterEditComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private riskRegistersService: RiskRegistersService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  public riskRegister: RiskRegister;
 
   ngOnInit(): void {
+    this.riskRegister = new RiskRegister();
+    this.getRegisterId();
+    this.getRegister();
+  }
+
+  getRegisterId() {
+    this.route.paramMap.subscribe(params => {
+      this.riskRegister.id = +params.get('id');
+    });
+  }
+
+  getRegister() {
+    this.riskRegister = this.riskRegistersService.getEditDetails(this.riskRegister.id);
+  }
+
+  public save(): void {
+    this.riskRegistersService.updateRegister(this.riskRegister.id, this.riskRegister.projectId, this.riskRegister.name, this.riskRegister.description);
+    this.router.navigate(['/registers/details/' + this.riskRegister.id]);
   }
 
 }
