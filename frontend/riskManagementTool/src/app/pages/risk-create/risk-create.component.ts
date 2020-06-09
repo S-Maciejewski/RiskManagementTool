ad1b5b10d970417036b5b68599d2c66a17cca225
PiterPSP <piter-h20@wp.pl>
2020-06-10 01:49:14 +0200

    implemented creating risk

--- frontend/riskManagementTool/src/app/pages/risk-create/risk-create.component.ts
+++ frontend/riskManagementTool/src/app/pages/risk-create/risk-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RisksService } from 'src/app/services/risks.service';
+import { Risk } from 'src/app/model/risk';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
   selector: 'app-risk-create',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RiskCreateComponent implements OnInit {
 
-  constructor() { }
+  public risk: Risk;
+  public impacts: string[];
+  public probabilities: string[];
+  public severities: string[];
+
+  constructor(
+    private risksService: RisksService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private storeService: StoreService
+  ) { }
 
   ngOnInit(): void {
+    this.risk = new Risk();
+    this.risk.dateRaised = new Date();
+    this.route.paramMap.subscribe(params => {
+      this.risk.registerId = +params.get('registerId');
+    });
+    this.getEnums();
+  }
+
+  getEnums() {
+    this.impacts = this.storeService.impacts;
+    this.probabilities = this.storeService.probability;
+    this.severities = this.storeService.severity;
+  }
+
+  public create(): void {
+    this.risksService.createRisk(this.risk);
+    this.router.navigate(['/registers/details', this.risk.registerId]);
   }
 
 }
